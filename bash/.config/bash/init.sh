34f05ad7e980a0bb5877bdb353cf8b4253adc002
Simon McLean <simonmclean@Simons-MacBook-Pro-2.local>
2025-12-19 17:57:57 +0000

    bash: add starship and carapace

--- bash/.config/bash/init.sh
+++ bash/.config/bash/init.sh
@@ -1,14 +1,18 @@
 if command -v zoxide &> /dev/null; then
-  eval "$(zoxide init bash)"
+  eval "$(zoxide init zsh)"
+fi
+
+if command -v starship &> /dev/null; then
+  eval "$(starship init zsh)"
+fi
+
+if command -v carapace &> /dev/null; then
+  export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
+  source <(carapace _carapace)
 fi
 
 if command -v fzf &> /dev/null; then
-  if [[ -f /usr/share/fzf/completion.bash ]]; then
-    source /usr/share/fzf/completion.bash
-  fi
-  if [[ -f /usr/share/fzf/key-bindings.bash ]]; then
-    source /usr/share/fzf/key-bindings.bash
-  fi
+  source <(fzf --zsh)
 fi
 
 source "$HOME/.config/bash/variables.sh"
