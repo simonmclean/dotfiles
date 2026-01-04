a79dc1fc1b38b5cc4f16aa0a553c8706fe00f001
Simon McLean <hello@simonmclean.dev>
2026-01-04 17:01:16 +0000

    bash: various updates

--- bash/.config/bash/init.sh
+++ bash/.config/bash/init.sh
@@ -9,7 +9,10 @@ if command -v starship &> /dev/null; then
 fi
 
 if command -v carapace &> /dev/null; then
-  export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
+  autoload -Uz compinit
+  compinit
+  export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
+  zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
   source <(carapace _carapace)
 fi
 
@@ -19,3 +22,4 @@ fi
 
 source "$HOME/.config/bash/variables.sh"
 source "$HOME/.config/bash/aliases.sh"
+source "$HOME/.config/bash/nvm.sh"
--- bash/.config/bash/variables.sh
+++ bash/.config/bash/variables.sh
@@ -2,4 +2,10 @@ if command -v nvim &> /dev/null; then
   export EDITOR="nvim"
 fi
 
+export PNPM_HOME="/Users/simonmclean/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
 export HOMEBREW_BUNDLE_FILE=~/.Brewfile
